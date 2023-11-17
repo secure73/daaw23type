2b760603f203794bb88fa5749cabc304d239e41c
Roboxon <145359440+roboxon@users.noreply.github.com>
2023-11-17 10:09:53 +0100

    accept incomming change Header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-
-const Header = () => {
-  return <div>Header</div>;
-=======
 import React from "react";
 import Navbar from "./Navbar";
 
@@ -11,7 +5,6 @@ const Header = () => {
   return (
         <Navbar/>
   )
->>>>>>> 76c5bcf43eab5225fe87f9076660e4d0a7b34ca3
 };
 
 export default Header;
