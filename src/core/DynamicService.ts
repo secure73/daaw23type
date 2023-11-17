1d07996c6abbdadd718941ec687226dbb196d3f9
Roboxon <145359440+roboxon@users.noreply.github.com>
2023-11-17 11:45:11 +0100

    Axios Wrapper and Toastify

---
+++ src/core/DynamicService.ts
@@ -0,0 +1,51 @@
+import ApiService from "./HttpService";
+import { toast } from "react-toastify";
+
+interface DynamicServiceProps {
+  service: string;
+  payload?: Object;
+  toastify?: Boolean;
+  toastifyMessage?: string;
+  file?: any;
+}
+
+const DynamicService = async (props: DynamicServiceProps) => {
+  const {
+    service,
+    payload,
+    toastifyMessage = "successfull",
+    toastify = true,
+    file,
+  } = props;
+
+  let servicePayload;
+  servicePayload = { service };
+
+  payload &&
+    (servicePayload = {
+      service,
+      payload,
+    });
+
+  servicePayload = JSON.stringify(servicePayload);
+  try {
+    let myForm = new FormData();
+    myForm.append("request", servicePayload);
+    if (file) {
+      myForm.append("file", file);
+    }
+
+    let data = await ApiService.post("localhost", myForm).then(
+      (response) => {
+        toastify === true && toast.success(toastifyMessage);
+        return response.data;
+      }
+    );
+    return data;
+  } catch (ex: any) {
+    toastify === true && toast.error(ex.response?.data?.service_message);
+    return false;
+  }
+};
+
+export default DynamicService;
---
+++ src/core/HttpService.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+axios.defaults.headers.post["Content-Type"] = "x-www-form-urlencoded";
+axios.interceptors.request.use(async (request) => {
+  // document.body.classList.add('loading-indicator');
+  let token = localStorage.getItem("loginToken");
+  if (token) {
+    request.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return request;
+});
+
+axios.interceptors.response.use(
+  function (response) {
+    // document.body.classList.remove('loading-indicator');
+    return response;
+  },
+  function (error) {
+    // document.body.classList.remove('loading-indicator');
+    toast.error(error.response?.data?.error);
+    throw error;
+  }
+);
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  post: axios.post,
+};
