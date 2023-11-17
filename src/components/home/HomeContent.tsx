199b97b66c07c7250556294fb9bb302d3a9e1e96
Roboxon <145359440+roboxon@users.noreply.github.com>
2023-11-17 09:31:03 +0100

    HomeContent.tsx conflict

--- src/components/home/HomeContent.tsx
+++ src/components/home/HomeContent.tsx
@@ -2,13 +2,6 @@ import React from 'react'
 
 const HomeContent = () => {
   return (
-<<<<<<< HEAD
-    <div>HomeContent</div>
-  )
-}
-
-export default HomeContent
-=======
     <div>
       
     </div>
@@ -16,4 +9,3 @@ export default HomeContent
 }
 
 export default HomeContent
->>>>>>> e1f5480349f946445b1246e9b8ee1665f03a14aa
