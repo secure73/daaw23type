2250db86abca57582369c84b3958bbaa8b9bafae
Roboxon <145359440+roboxon@users.noreply.github.com>
2023-11-17 10:16:25 +0100

    resove conflict with dev

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import React from 'react';
-import Footer from './Footer';
-
-const MainLayout = () => {
-  return <div>MainLayout</div>;
-};
-=======
 import React, { Fragment } from 'react'
 import Header from './Header'
+import Footer from './Footer'
 
 type MainLayoutProps = {
     children: React.ReactNode
@@ -17,9 +10,9 @@ const MainLayout = (props:MainLayoutProps) => {
     <Fragment>
      <Header />
       {props.children}
+      <Footer/>
     </Fragment>
   );
 }
->>>>>>> 76c5bcf43eab5225fe87f9076660e4d0a7b34ca3
 
 export default MainLayout;
